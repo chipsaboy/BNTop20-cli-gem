f8637abc8338757fdc192fece7a1f24e326451aa
Nathan Campbell <nathancampbell@outlook.com>
2017-05-31 15:16:08 -0500

    starting book and scraper

--- BNTop20/lib/BNTop20/book.rb
+++ BNTop20/lib/BNTop20/book.rb
@@ -1,6 +1,26 @@
 class BNTop20::Book
 
-	attr_accessor :name, :author, :rating, :release_date, :price, :old_price, :discount
+	attr_accessor :name, :author, :price, :url, :release_date, :old_price, :discount, :rating
+
+	@@all = []
+
+	def initialize(name = nil, author = nil, price = nil, url = nil)
+		@name = name
+		@author = author
+		@price = price
+		@url = url
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.find(i)
+		self.all[i-1]
+	end
+
+
 
 
 end
--- BNTop20/lib/BNTop20/scraper.rb
+++ BNTop20/lib/BNTop20/scraper.rb
@@ -1,17 +1,12 @@
 class BNTop20::Scraper
 
+	def get_html
+		Nokogiri::HTML(open("https://www.barnesandnoble.com/b/books/_/N-1fZ29Z8q8?Nrpp=20&page=1"))
+	end
 
-
-
-
-
-
-
-
-
-
-
-
+	def scrape_html
+		self.get_html.css("ul#listView")
+	end
 
 
 end
